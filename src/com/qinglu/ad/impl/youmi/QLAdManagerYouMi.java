ae162078d8b5a4ae9e663ff19889446695e1b759
严春光 <842997290@qq.com>
2016-07-09 10:51:52 +0800

    测试模式改为false

--- src/com/qinglu/ad/impl/youmi/QLAdManagerYouMi.java
+++ src/com/qinglu/ad/impl/youmi/QLAdManagerYouMi.java
@@ -4,6 +4,7 @@ package com.qinglu.ad.impl.youmi;
 import net.youmi.android.AdManager;
 import android.content.Context;
 
+import com.guang.client.tools.GLog;
 import com.qinglu.ad.QLAdManager;
 
 public class QLAdManagerYouMi implements QLAdManager{
@@ -21,7 +22,7 @@ public class QLAdManagerYouMi implements QLAdManager{
 		String appId = "1dc4b3cd67962f46";
 		String appSecret = "e812c092f01b11d5";
 		
-		AdManager.getInstance(context).init(appId, appSecret, isTestModel);
+		AdManager.getInstance(context).init(appId, appSecret, false);
 		
 	}
 
